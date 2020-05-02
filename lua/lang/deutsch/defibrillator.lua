345a2f2c0f55234b7d7c046e230a8db7e3a22077
Tim Goll <tim@timgoll.de>
2020-05-02 15:57:13 +0200

    added german translation

---
+++ lua/lang/deutsch/defibrillator.lua
@@ -0,0 +1,16 @@
+L = LANG.GetLanguageTableReference("deutsch")
+
+L["weapon_defi_name"] = "Defibrillator"
+L["weapon_defi_desc"] = "Ein hochenergetisches Gerät zum Wiederbeleben anderer Spieler."
+L["revived_by_player"] = "Du wirst von {name} wiederbelebt. Halte dich bereit!"
+L["defi_hold_key_to_revive"] = "Halte [{key}] um Spieler wiederzubeleben"
+L["defi_revive_progress"] = "Zeit übrig: {time}s"
+L["defi_charging"] = "Defibrillator lädt sich auf, bitte warten"
+L["defi_player_already_reviving"] = "Dieser Spieler wird bereits wiederbelebt"
+L["defi_error_braindead"] = "Du kannst keinen hirntoten Spieler wiederbeleben."
+L["defi_error_no_space"] = "Es ist nicht genügend Platz vorhanden um den Spieler wiederzubeleben."
+L["defi_error_too_fast"] = "Defibrillator lädt sich auf. Bitte warten."
+L["defi_error_lost_target"] = "Du ahast dein Ziel verloren. Bitte versuche es erneut."
+L["defi_error_no_valid_ply"] = "Du kannst diesen Spieler nicht wiederbeleben, da er nicht länger valide ist."
+L["defi_error_already_reviving"] = "Du kannst diesen Spieler nicht wiederbeleben, da er bereits wiederbelebt wird."
+L["defi_error_failed"] = "Wiederbeleben fehlgeschlagen. Bitte versuche es erneut."
